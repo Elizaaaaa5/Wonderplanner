1b8f774edc5f1d8131b653b9dec6f6031f489f8d
reginayap <35645652+reginayap@users.noreply.github.com>
2018-02-04 18:27:08 -0500

    Update index.php
    Inserted header file

--- Wonderplanner/wonderplanner/index.php
+++ Wonderplanner/wonderplanner/index.php
@@ -20,42 +20,7 @@
 			<!-- Header -->
 				<div id="header-wrapper" class="wrapper">
 					<div id="header">
-
-						<!-- Logo -->
-							<div id="logo">
-								<h1><a href="index.html">Escape Velocity</a></h1>
-								<p>A free responsive site template by HTML5 UP</p>
-							</div>
-
-						<!-- Nav -->
-							<nav id="nav">
-								<ul>
-									<li class="current"><a href="index.html">Home</a></li>
-									<li>
-										<a href="About_us.php">About Us</a>
-										<ul>
-											<li><a href="#">Lorem ipsum</a></li>
-											<li><a href="#">Magna veroeros</a></li>
-											<li><a href="#">Etiam nisl</a></li>
-											<li>
-												<a href="#">Sed consequat</a>
-												<ul>
-													<li><a href="#">Lorem dolor</a></li>
-													<li><a href="#">Amet consequat</a></li>
-													<li><a href="#">Magna phasellus</a></li>
-													<li><a href="#">Etiam nisl</a></li>
-													<li><a href="#">Sed feugiat</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Nisl tempus</a></li>
-										</ul>
-									</li>
-									<li><a href="Sign_up.php">Sign Up</a></li>
-									<li><a href="Login.php">Login</a></li>
-									<li><a href="Contact_us.php">Contact Us</a></li>
-								</ul>
-							</nav>
-
+					   <?php include("header.php)");?>
 					</div>
 				</div>
 
