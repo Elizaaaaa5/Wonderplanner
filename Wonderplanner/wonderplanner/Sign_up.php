92decc229f906704d4746bc1716ee51eaec95635
Elizaaaaa5 <elizaclu5@gmail.com>
2018-02-23 12:44:48 -0500

    updated server.php

--- Wonderplanner/wonderplanner/Sign_up.php
+++ Wonderplanner/wonderplanner/Sign_up.php
@@ -30,19 +30,6 @@
 			<link rel="stylesheet" type="text/css" href="assets/Login_v1/css/util.css">
 			<!--<link rel="stylesheet" type="text/css" href="assets/Login_v1/css/main.css">
 		<!==============================================================================================-->
-	<!--	
-		    // if(isset($_POST['submit'])) {
-		      // $sbm = $conn->prepare("INSERT INTO users (FIRST_NAME, LAST_NAME, EMAIL, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-		      // $hashpass = password_hash($_POST['contact_password'], PASSWORD_DEFAULT);
-		      // $sbm->bind_param("ssssssss", $_POST['contact_name'], $_POST['contact_lname'], $_POST['contact_email'], $_POST['contact_address'], $_POST['contact_city'], $_POST['contact_state'], $_POST['contact_zipcode'], $hashpass);
-		      // $sbm->execute();
-<<<<<<< HEAD
-
-=======
->>>>>>> d07fc77012da967ee1fcfe27a1de82577a0cfb2d
-
-		  // }
-		  //  --!>
 	</head>
 	<body class="homepage">
 		<div id="page-wrapper">
@@ -83,8 +70,7 @@
 								<div class="row">
 									<div class="12u">
 										<div <?php if (isset($email_error)): ?> class="form_error" <?php endif ?> >
-										<label for="email" align="left">Email<font color="red">*</font></label>
-										<input type="text"  pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" onchange= "validateEmail()" name="contact_email" id="contact_email" placeholder="you@example.domain" title="you@example.domain"required>
+										<input type="text"  pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" onchange= "the_function()" name="contact_email" id="contact_email" placeholder="you@example.domain" title="you@example.domain"required>
 									</div>
 									<?php if (isset($email_error)): ?>
 										<span><?php echo $email_error; ?></span>
@@ -261,65 +247,6 @@
 				
 			<script>
 
-<<<<<<< HEAD
-=======
-				<script>
-				var pass = document.getElementById("contact_password");
-				var letter = document.getElementById("letter");
-				var capital = document.getElementById("capital");
-				var number = document.getElementById("number");
-				var length = document.getElementById("length");
-				// When the user clicks on the password field, show the message box
-				pass.onfocus = function() {
-				  document.getElementById("message").style.display = "block";
-				}
-				// When the user clicks outside of the password field, hide the message box
-				pass.onblur = function() {
-				  document.getElementById("message").style.display = "none";
-				}
-				// When the user starts to type something inside the password field
-				pass.onkeyup = function() {
-				  // Validate lowercase letters
-				  var lowerCaseLetters = /[a-z]/g;
-				  if(pass.value.match(lowerCaseLetters)) {
-				    letter.classList.remove("invalid");
-				    letter.classList.add("valid");
-				  } else {
-				    letter.classList.remove("valid");
-				    letter.classList.add("invalid");
-				}
-				  // Validate capital letters
-				  var upperCaseLetters = /[A-Z]/g;
-				  if(pass.value.match(upperCaseLetters)) {
-				    capital.classList.remove("invalid");
-				    capital.classList.add("valid");
-				  } else {
-				    capital.classList.remove("valid");
-				    capital.classList.add("invalid");
-				  }
-				  // Validate numbers
-				  var numbers = /[0-9]/g;
-				  if(pass.value.match(numbers)) {
-				    number.classList.remove("invalid");
-				    number.classList.add("valid");
-				  } else {
-				    number.classList.remove("valid");
-				    number.classList.add("invalid");
-				  }
-				  // Validate length
-				  if(pass.value.length >= 8) {
-				    length.classList.remove("invalid");
-				    length.classList.add("valid");
-				  } else {
-				    length.classList.remove("valid");
-				    length.classList.add("invalid");
-				  }
-				}
-				</script>
-				
-			<script>
-
->>>>>>> d07fc77012da967ee1fcfe27a1de82577a0cfb2d
 				(function() {
 				var pwd = document.getElementById("contact_password");
 				var show = document.getElementById("showPwd");
@@ -355,19 +282,19 @@
 				}());
 
 			</script>
-			<script>
-					var user_email = document.getElementById("contact_email");
-				
-					function validateEmail(){
-					  $user_check_query = "SELECT * FROM users WHERE contact_email='$EMAIL' LIMIT 1";
-					  var result = mysqli_query('phpmyadmin', $user_check_query);
-					  //$count = mysqli_num_rows($result);
-					  
-					  if (user_email === result) { // if user exists
-						echo 'Sorry, this email is already taken!';
+			<script type="text/javascript">
+				function the_function(){
+						$.ajax({
+							type: 'POST',
+							url: 'server.php',
+							success: function(checkemail) {
+								alert(checkemail);
+								$("p").text(checkemail);
+
+							}
 						}
-					  }
-					}
+					});
 			</script>
+
 	</body>
 </html>
--- Wonderplanner/wonderplanner/server.php
+++ Wonderplanner/wonderplanner/server.php
@@ -38,17 +38,17 @@
   	
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
- 
- $user_check_query = "SELECT * FROM users WHERE contact_email='$EMAIL' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-  
-  if ($user) { // if user exists
-    if ($user['contact_email'] === $EMAIL) {
-      array_push($errors, "email already exists");
+ function checkemail(){
+    $user_check_query = "SELECT * FROM users WHERE contact_email='$EMAIL' LIMIT 1";
+      $result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($result);
+      
+      if ($user) { // if user exists
+        if ($user['contact_email'] === $EMAIL) {
+          array_push($errors, "email already exists");
+        }
+      }
     }
-  }
-<<<<<<< HEAD
 
   	// $sql_e = "SELECT * FROM users WHERE contact_email ='$EMAIL'";
   	// $res_e = mysqli_query($conn, $sql_e);
@@ -81,102 +81,3 @@
 
   $conn->close();
   
-
-  //  if(isset($_POST["submit"]))
-  //   {
-  //       $error = 0;
-  //       if ((isset($_POST['contact_name']) && !empty($_POST['contact_name']))||
-	// 			(isset($_POST['contact_lname']) && !empty($_POST['contact_lname']))) {
-	// 				$FIRST_NAME=mysqli_real_escape_string($conn,trim($_POST['contact_name']));
-  //         $LAST_NAME=mysqli_real_escape_string($conn,trim($_POST['contact_lname']));
-  //       }else{
-  //           $error = 1;
-  //           $empty_username="Username Cannot be empty.";
-  //           echo $empty_username.'<br>';
-  //       }
-  //       if (isset($_POST['contact_email']) && !empty($_POST['contact_email'])) {
-  //           $EMAIL=mysqli_real_escape_string($conn,trim($_POST['contact_email']));
-  //       }else{
-  //           $error =1;
-  //           $empty_email="Email cannot be empty.";
-  //           echo $empty_email.'<br>';
-  //       }
-
-  //       if (isset($_POST['contact_password']) && !empty($_POST['contact_password'])) {
-  //           $psw=mysqli_real_escape_string($conn,trim($_POST['contact_password']));
-  //       }else{
-  //           $error = 1;
-  //           $empty_password="Password cannot be empty";
-  //           echo $empty_password.'<br>';
-  //       }
-
-  //       if (isset($_POST['contact_password2']) && !empty($_POST['contact_password2'])) {
-  //           $PASSWORD2=mysqli_real_escape_string($conn,trim($_POST['contact_password2']));
-  //       }else{
-  //           $error = 1;
-  //           $empty_repassword="Retype password cannot be empty";
-  //           echo $empty_repassword.'<br>';
-  //       }
-
-  //       $PASSWORD=password_hash('$psw',PASSWORD_BCRYPT);
-  //       $date=mysqli_real_escape_string($conn, trim('now()'));
-  //       if($psw!=$PASSWORD2)
-  //       {
-  //           echo "password not Matched";
-  //       }
-
-  //       if(!$error) {
-  //           $sql="SELECT * from users WHERE (contact_email='$EMAIL');";
-	// 					$res=mysqli_query($conn,$sql);
-  //           // if (sqli_num_rows($res) > 0) {
-  //           // output data of each row
-	// 					$row = mysqli_fetch_assoc($res);
-  //           // <!-- if ($username==$row['username'])
-  //           // {
-  //           //     echo "Username already exists";
-  //           // } -->
-  //           if($row==$row['EMAIL'])
-  //           {
-  //               echo "Email already exists";
-  //           }
-  //       }else { //here you need to add else condition
-  //           echo "alright";
-  //       }
-  //       // }
-  //   }
-	// 	$conn->close();
-  //   ?>
-=======
-
-  	// $sql_e = "SELECT * FROM users WHERE contact_email ='$EMAIL'";
-  	// $res_e = mysqli_query($conn, $sql_e);
-
-  	// if(mysqli_num_rows($res_e) > 0){
-  	  // $email_error = "Sorry, this email already taken!"; 	
-  	// }else{
-           // exit();
-	// }
-    // Finally, register user if there are no errors in the form
-    if (count($errors) == 0) {
-    	// $PASSWORD = md5($PASSWORD);//encrypt the PASSWORD before saving in the database
-
-    	$sbm = $conn->prepare("INSERT INTO users (FIRST_NAME, LAST_NAME, EMAIL, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-		$hashpass = password_hash($_POST['contact_password'], PASSWORD_DEFAULT);
-		$sbm->bind_param("ssssssss", $_POST['contact_name'], $_POST['contact_lname'], $_POST['contact_email'], $_POST['contact_address'], $_POST['contact_city'], $_POST['contact_state'], $_POST['contact_zipcode'], $hashpass);
-		$sbm->execute();
-    	// $_SESSION['success'] = "You are now logged in";
-		echo("Account created successfully!");
-    	header('location: server.php');
-    }
-
-
-  if ($conn->query(sql) === TRUE) {
-      echo "New record created successfully";
-  } else {
-      echo "Error: " . sql . "<br>" . $conn->error;
-  }
-}
-
-  $conn->close();
-  ?>
->>>>>>> d07fc77012da967ee1fcfe27a1de82577a0cfb2d
