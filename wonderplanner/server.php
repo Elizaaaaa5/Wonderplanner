e9924003febb23d5d56e3ca49bc3051c655d96e2
Luyao <elizaclu5@gmail.com>
2018-03-17 17:03:02 -0400

    Update server.php

--- wonderplanner/server.php
+++ wonderplanner/server.php
@@ -31,43 +31,31 @@
 	  $STATE = $_POST['contact_state'];
 	  $ZIPCODE = $_POST['contact_zipcode'];
   	
-      // first check the database to make sure a user does not already exist with the same username and/or email
-	   function checkemail(){
-		  // $user_check_query = "SELECT count(*) FROM users WHERE EMAIL = 'contact_email' LIMIT 1";
-		  // $result = mysqli_query($db, $user_check_query);
-		  // $user = mysqli_fetch_assoc($result);
-		  
-		  // if ($user_check_query > 0) { // if user exists
-		   // if ($user['contact_email'] === $EMAIL) {
-			  // array_push($errors, "This email already is already taken. Please use a different email.");
-		   // }
-		  // }
-		  
-			if (!get_magic_quotes_gpc()) { 
-			$_POST['contact_email'] = addslashes($_POST['contact_email']); 
-			} 
-			$emailcheck = $_POST['contact_email']; 
-			$check     = mysql_query("SELECT EMAIL FROM users WHERE EMAIL = '$emailcheck'")  
-			or die(mysql_error()); 
-			$check2    = mysql_num_rows($check); 
-
-			//  if the name exists it gives an error 
-			if ($check2 != 0) { 
-			die('Sorry, the email '.$_POST['contact_email'].' is already in use.'); 
-			}
-	   }
+	  // first check the database to make sure a user does not already exist with the same username and/or email
 	  
-	  
-  	// $sql_e = "SELECT * FROM users WHERE contact_email ='$EMAIL'";
-  	// $res_e = mysqli_query($conn, $sql_e);
 
-  	// if(mysqli_num_rows($res_e) > 0){
-  	  // $email_error = "Sorry, this email already taken!"; 	
-  	// }else{
-           // exit();
-	// }
-    // Finally, register user if there are no errors in the form
-    if (count($errors) == 0) {
+	  $emailfound=false;
+
+     $sql='select `EMAIL` from `users` where `EMAIL`=?';
+    $stmt=$conn->prepare( $sql );
+    if( $stmt ){
+
+        $stmt->bind_param("s",$_POST['contact_email']);
+        $result=$stmt->execute();
+        if( $result ){
+            $stmt->store_result();
+            $stmt->bind_result( $emailfound );
+            $stmt->fetch();
+            $stmt->free_result();
+        }
+    }
+
+    if( $emailfound ){
+        echo 'Sorry, that email address already exists in our database. Please try again with a different address.';
+        $stmt->close();
+        $conn->close();
+
+    } else {
     	// $PASSWORD = md5($PASSWORD);//encrypt the PASSWORD before saving in the database
 
     	$sbm = $conn->prepare("INSERT INTO users (FIRST_NAME, LAST_NAME, EMAIL, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
@@ -76,10 +64,9 @@
 		$sbm->execute();
     	// $_SESSION['success'] = "You are now logged in";
 		echo("Account created successfully!");
-    	header('location: Sign_up.php');
-    }else{
-		echo $errors[0];
+    	header('location: Login.php');
 	}
+
 
 	if(isset($_POST['contact_email'])) {
  
