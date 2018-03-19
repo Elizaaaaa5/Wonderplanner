4c4ab9456e5bda7fa81af334b2d1d04446d803ad
Luyao <elizaclu5@gmail.com>
2018-03-19 12:59:22 -0400

    changed button to match prices

--- wonderplanner/Our_products.php
+++ wonderplanner/Our_products.php
@@ -58,10 +58,10 @@
 										<p>0.07 BTC</p>
 										<ul class="actions">
 											<form action="https://test.bitpay.com/checkout" method="post" >
-											  <input type="hidden" name="action" value="checkout" />
-											  <input type="hidden" name="posData" value="" />
-											  <input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVW1+CxP0xyEGvzhAOBZCqY1E31Z5j7g3lVftqlXET4whTGpifw89uXLcuJ7c/BAqxpsb2SokBMWvXjSXAcIAf343RBLgDRiFEX3ABUpYSQqGTwVw7XLxQHcePpagHgTJ8cc+xJ94L2zZJ3Zd6Cgdn3oPzblLqeFNmdkxle+FsMbySzsUWGoiMhMMUtfjKTnqwqJxXuvuJivDv18pevSL4uqgoa6BX+cvlsSim+C9VNPxm3uYts7rPp9DDF6hMVF1EWKv2ltxuoEL8lEOIE2jQfVBliPPH7AIk+l9rkrroRtXxLGniczPf10iMUhqNcH7cCDgq+xNLdchIecaOwc4FcR+iAG8g/1HAErbSjkuoZCTrrsOUBEAG4F6JqzLfFsLNXaHtq4CoMxMGlcSYAW3fg==" />
-											  <input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
+												<input type="hidden" name="action" value="checkout" />
+												<input type="hidden" name="posData" value="" />
+												<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVW1+CxP0xyEGvzhAOBZCqY1E31Z5j7g3lVftqlXET4whTGpifw89uXLcuJ7c/BAqxpsb2SokBMWvXjSXAcIAf343RBLgDRiFEX3ABUpYSQqGTwVw7XLxQHcePpagHgTJ8cc+xJ94L2zZJ3Zd6Cgdn3oPzblLqeFNmdkxle+FsMbySzsUWGoiMhMMUtfjKTnqwqJxXuvuJivDv18pevSL4uqgoa6BX+cvlsSim+C9VNPxm3uYts7rPp9DDF6hMVF1EWKv2ltxuoEL8lEOIE2jQfVBliPPH7AIk+l9rkrroRtXxLGniczPf10iMUhqNcH7NSKDIOnSLSMa46DWk3zo8M3fjq6LVILmm7yMsz8BCA/iAH8++k5dSOY8KngInCuL" />
+												<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
 											</form>
 										</ul>
 									</section>
@@ -74,10 +74,10 @@
 										<p>0.08 BTC</p>
 										<ul class="actions">
 											<form action="https://test.bitpay.com/checkout" method="post" >
-											  <input type="hidden" name="action" value="checkout" />
-											  <input type="hidden" name="posData" value="" />
-											  <input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVq1TIrcvebdMR4HwFOQRM1SjgXWX/9465HfxcseRoaWATPMaTkYUrHa8bBCG4L29guv+L0b6jNpCjNpJH4Hnpe7jZLsCcyoHMTdcs2JqioTRSNMzx4MFrRzOCD2cteiy0SMLSlA3+sSOLIxrn6QMvjO8B4OXfBHA8Hf8IvezwT18RnzEq/mpBfFowHpX6jCc2eP96ULvF8UDUp0i0j14RNeEx25jwCUUAO0liMOgQ4s97YN7pkuWGhsdLz5R/jBYh7gvGPlbULGFxIoNwyeNCGrtLAlPaM2kW+Zlls4Damsr1zcWO9txGvchi/fndzTBakB4KxP6YeVIBSnXlRnIAI+qh17WGYOENnPLpa9WAWmHcfX0TnqqiMXMUsDxDMn5iBBHqK05pHTff3XF+H/ZeQ==" />
-											  <input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
+												<input type="hidden" name="action" value="checkout" />
+												<input type="hidden" name="posData" value="" />
+												<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVq1TIrcvebdMR4HwFOQRM1SjgXWX/9465HfxcseRoaWATPMaTkYUrHa8bBCG4L29guv+L0b6jNpCjNpJH4Hnpe7jZLsCcyoHMTdcs2JqioTRSNMzx4MFrRzOCD2cteiy0SMLSlA3+sSOLIxrn6QMvjO8B4OXfBHA8Hf8IvezwT18RnzEq/mpBfFowHpX6jCc2eP96ULvF8UDUp0i0j14RK1jPfqqvfFslgKX1PIsutmItaZnRiJi3mAt6Kc4hiwLcE+IdU03tOPM2n3DVzA0NgorTfJCf8Y2zHUxLpGnSK5YZ6lOy1spqXNF4BN5opxh+Y3iDMPO/SDz8rBHnpJMb90Y3k7f64ofao8dNviXngL2c5OT9UFSsO0af1O7yB0JH2pFDuG1WQ5V9+XGMNXu7Q==" />
+												<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
 											</form>
 										</ul>
 									</section>
@@ -89,12 +89,12 @@
 										<p>Fly to the conference and sleep at the top of a 5 star hotel.</p>
 										<p>0.10 BTC</p>
 										<ul class="actions">
-											<form action="https://test.bitpay.com/checkout" method="post" >
-											  <input type="hidden" name="action" value="checkout" />
-											  <input type="hidden" name="posData" value="" />
-											  <input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIpINdijW0tAeTOyYyqcpiS509EvuFe7OZZCvh7Bb4V3ZEDI4BTu6DisjDhzbaXM6Ao7W/FxMaK++dzJmnO3UD2juvN61Hc0jdbMUkKQBckvSX4K1gcUnUQj/oly3XDSdkIeX4qkQUyDK60VALyt0L3USlt7GBixZqoROHI7X4vbGpQVjZ1m59p1TqEQQLuthyBp3rhzRQx2C+Ki6KlvKRejMuC1rocZDUJF1v9k+ecDFdiO5tY/eikcETL+gQN8qO2jJXdENSa+wDaVWfRw3km3LckQKJOaLZ/wGqca+Nm8WbunTazOeBc3/1cm9qbwHZBWv/7s+lZH7Zn//9YZ87l9fv8/ffK8/W+LqhiUVHm9LA9RW9fEDNJSuYnOPRI2iDQzm/x1oBp32h4pkm/UaDPkaJJyVHI+rqY+dP3PdZTAAzjViYPi7GieyrvCngM3pvlvf4MRwQXqkplXcZZtwai2qYYC8nkv1Yp6hOlSEhvZk=" />
-											  <input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-											</form>
+										<form action="https://test.bitpay.com/checkout" method="post" >
+											<input type="hidden" name="action" value="checkout" />
+											<input type="hidden" name="posData" value="" />
+											<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIpINdijW0tAeTOyYyqcpiS509EvuFe7OZZCvh7Bb4V3ZKaNT1UDRzdriyp6kbvURS/MkIttvP3rLNTwCgyS+wHpwErdZ1adEgRf7rWhNre+VkB2S47gAeCQPu34BPjFEbYdf4ETf8G+8IV6u8n2c21e7jx58a49c8kH7V/2kXOl8laAydxs+lFLxxKlYWht0TtaVEKNVzPWqitbWkD5b3QqKlJcO4gzabgH9OWG5GOfyUBC0u5w+90IQhJfplFMq8dOJC/yfbihfXR8h3I+5Vh+FZHXHgkQlPsydaOQvHr7JgxmlUpTCjDy41vChUi1q1APdA9AuFu9dhCg/mHqPpxlGbluFO7rA3bGrtPvHebLu18lqxby4+ZTP0Rm6eN4a4" />
+											<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
+										</form>
 										</ul>
 									</section>
 								</div>
@@ -113,9 +113,6 @@
 					</div>
 					
 		</section>
-		<br/>
-		<br/>
-		<br/>
 		<br/>
 		<br/>
 		<br/>
