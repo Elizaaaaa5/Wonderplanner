2e0f25f1bfee51a3a059930994b2214450ad80fd
Luyao <elizaclu5@gmail.com>
2018-03-19 12:59:56 -0400

    updated messages

--- wonderplanner/Our_products.php
+++ wonderplanner/Our_products.php
@@ -1,160 +1,125 @@
-<!DOCTYPE HTML>
-<!--
-	Escape Velocity by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Wonder Planner</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<!--===============================================================================================-->
-		<!--<link rel="icon" type="image/png" href="assets/Login_v1/images/icons/favicon.ico"/>-->
-		<!--===============================================================================================-->
-		<!--<link rel="stylesheet" type="text/css" href="assets/Login_v1/vendor/bootstrap/css/bootstrap.min.css">
-		<!===============================================================================================-->
-			<!--<link rel="stylesheet" type="text/css" href="assets/Login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css">-->
-		<!--===============================================================================================-->
-			<link rel="stylesheet" type="text/css" href="assets/Login_v1/vendor/animate/animate.css">
-		<!--===============================================================================================-->
-			<link rel="stylesheet" type="text/css" href="assets/Login_v1/vendor/css-hamburgers/hamburgers.min.css">
-		<!--===============================================================================================-->
-			<link rel="stylesheet" type="text/css" href="assets/Login_v1/vendor/select2/select2.min.css">
-		<!--===============================================================================================-->
-			<link rel="stylesheet" type="text/css" href="assets/Login_v1/css/util.css">
-			<!--<link rel="stylesheet" type="text/css" href="assets/Login_v1/css/main.css">
-		<!==============================================================================================-->
-	</head>
-	<body class="homepage">
-		<div id="page-wrapper">
+<body bgcolor="#F5C750"><section align="center"><font color="#F5E550"><strong><?php
 
-			<!-- Header -->
-				<div id="header-wrapper" class="wrapper">
-					<div id="header">
-					   <?php include("header.php");?>
-					</div>
-				</div>
-		<div class="wrapper style2">
-		<section>
-				<div class="title">Our Packages</div>
-										
-						<header class="style1" align="right">
-							<h2>Choose Your Perfect Day, Today!</h2>
-						</header>
-						<br/>
-						<br/>
-					
-						<div id="highlights" class="container">
-							<div class="row 150%">
-								<div class="4u 12u(mobile)">
-									<section class="highlight">
-										<a href="#" class="image featured"><img src="images/familytrip.jpg" alt="" /></a>
-										<h3><a href="#">Family Trip</a></h3>
-										<p>Enjoy a trip to your local zoo, a great picnic and the adventures!</p>
-										<p>0.07 BTC</p>
-										<ul class="actions">
-											<form action="https://test.bitpay.com/checkout" method="post" >
-												<input type="hidden" name="action" value="checkout" />
-												<input type="hidden" name="posData" value="" />
-												<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVW1+CxP0xyEGvzhAOBZCqY1E31Z5j7g3lVftqlXET4whTGpifw89uXLcuJ7c/BAqxpsb2SokBMWvXjSXAcIAf343RBLgDRiFEX3ABUpYSQqGTwVw7XLxQHcePpagHgTJ8cc+xJ94L2zZJ3Zd6Cgdn3oPzblLqeFNmdkxle+FsMbySzsUWGoiMhMMUtfjKTnqwqJxXuvuJivDv18pevSL4uqgoa6BX+cvlsSim+C9VNPxm3uYts7rPp9DDF6hMVF1EWKv2ltxuoEL8lEOIE2jQfVBliPPH7AIk+l9rkrroRtXxLGniczPf10iMUhqNcH7NSKDIOnSLSMa46DWk3zo8M3fjq6LVILmm7yMsz8BCA/iAH8++k5dSOY8KngInCuL" />
-												<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-											</form>
-										</ul>
-									</section>
-								</div>
-								<div class="4u 12u(mobile)">
-									<section class="highlight">
-										<a href="#" class="image featured"><img src="images/romantic.jpg" alt="" /></a>
-										<h3><a href="#">Romantic Date</a></h3>
-										<p>Start with a rose delivered to her door and tour the boardwalk after dinner.</p>
-										<p>0.08 BTC</p>
-										<ul class="actions">
-											<form action="https://test.bitpay.com/checkout" method="post" >
-												<input type="hidden" name="action" value="checkout" />
-												<input type="hidden" name="posData" value="" />
-												<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIVq1TIrcvebdMR4HwFOQRM1SjgXWX/9465HfxcseRoaWATPMaTkYUrHa8bBCG4L29guv+L0b6jNpCjNpJH4Hnpe7jZLsCcyoHMTdcs2JqioTRSNMzx4MFrRzOCD2cteiy0SMLSlA3+sSOLIxrn6QMvjO8B4OXfBHA8Hf8IvezwT18RnzEq/mpBfFowHpX6jCc2eP96ULvF8UDUp0i0j14RK1jPfqqvfFslgKX1PIsutmItaZnRiJi3mAt6Kc4hiwLcE+IdU03tOPM2n3DVzA0NgorTfJCf8Y2zHUxLpGnSK5YZ6lOy1spqXNF4BN5opxh+Y3iDMPO/SDz8rBHnpJMb90Y3k7f64ofao8dNviXngL2c5OT9UFSsO0af1O7yB0JH2pFDuG1WQ5V9+XGMNXu7Q==" />
-												<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-											</form>
-										</ul>
-									</section>
-								</div>
-								<div class="4u 12u(mobile)">
-									<section class="highlight">
-										<a href="#" class="image featured"><img src="images/business.jpg" alt="" /></a>
-										<h3><a href="#">Business Meeting</a></h3>
-										<p>Fly to the conference and sleep at the top of a 5 star hotel.</p>
-										<p>0.10 BTC</p>
-										<ul class="actions">
-										<form action="https://test.bitpay.com/checkout" method="post" >
-											<input type="hidden" name="action" value="checkout" />
-											<input type="hidden" name="posData" value="" />
-											<input type="hidden" name="data" value="qirB1JNjLtMG+AfKXIWK9vL7PlfX2iZLfANhXwQwlwWtEvajCvLJddyJGytj2gUIpINdijW0tAeTOyYyqcpiS509EvuFe7OZZCvh7Bb4V3ZKaNT1UDRzdriyp6kbvURS/MkIttvP3rLNTwCgyS+wHpwErdZ1adEgRf7rWhNre+VkB2S47gAeCQPu34BPjFEbYdf4ETf8G+8IV6u8n2c21e7jx58a49c8kH7V/2kXOl8laAydxs+lFLxxKlYWht0TtaVEKNVzPWqitbWkD5b3QqKlJcO4gzabgH9OWG5GOfyUBC0u5w+90IQhJfplFMq8dOJC/yfbihfXR8h3I+5Vh+FZHXHgkQlPsydaOQvHr7JgxmlUpTCjDy41vChUi1q1APdA9AuFu9dhCg/mHqPpxlGbluFO7rA3bGrtPvHebLu18lqxby4+ZTP0Rm6eN4a4" />
-											<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-										</form>
-										</ul>
-									</section>
-								</div>
-							</div>
-							<section align="center">
-								<header class="style1" align="right">
-									<h2>Bitcoin Converter Tools</h2>
-								</header>
-								<iframe frameBorder="0" scrolling="no" allowtransparency="0" src="https://bitcoinaverage.com/en/widgets?widgetType=price&bgcolor=#FFFFFF&bwidth=1&bcolor=#CCCCCC&cstyle=round&fsize=16px&ffamily=arial&fcolor=#000000&bgTransparent=solid&chartStyle=none&lastUpdateTime=block&currency0=USD&total=1" style="width:250px; height:150px; overflow:hidden; background-color:transparent !important;"></iframe>
-								<section align="center">
-								<iframe frameBorder="0" scrolling="no" allowtransparency="0" src="https://bitcoinaverage.com/en/widgets?widgetType=conversion&bgcolor=#FFFFFF&bwidth=1&bcolor=#CCCCCC&cstyle=round&fsize=16px&ffamily=arial&fcolor=#000000&bgTransparent=solid&chartStyle=none&lastUpdateTime=block&currency0=USD&total=1" style="width:250px; height:275px; overflow:hidden; background-color:transparent !important;"></iframe>
-							</section>
-								
-							</section>
-						</div>
-					</div>
-					
-		</section>
-		<br/>
-		<br/>
-		<br/>
-		
-			<!-- Footer -->
-				<div id="footer-wrapper" class="wrapper">
-				    <?php include("footer.php");?>
-				</div>
+	$conn = mysqli_connect("localhost", "root", "", "phpmyadmin");
+	
+	// Checking the  connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+		echo "connection failed";
+	}
 
-		</div>
+	// initializing variables
+	$FIST_NAME = "";
+	$LAST_NAME = "";
+	$EMAIL = "";
+	$PASSWORD = "";
+	$ADDRESS="";
+	$CITY="";
+	$STATE="";
+	$ZIPCODE="";
+	$errors=array();
+	
+	// REGISTER USER
+	if (isset($_POST['submit'])) {
+ 
+	  $FIRST_NAME = $_POST['contact_name'];
+	  $LAST_NAME = $_POST['contact_lname'];
+	  $EMAIL = $_POST['contact_email'];
+	  $PASSWORD = $_POST['contact_password'];
+	  $ADDRESS = $_POST['contact_address'];
+	  $CITY = $_POST['contact_city'];
+	  $STATE = $_POST['contact_state'];
+	  $ZIPCODE = $_POST['contact_zipcode'];
+  	
+      // first check the database to make sure a user does not already exist with the same username and/or email
+	  $emailfound=false;
 
+	  $sql='select `EMAIL` from `users` where `EMAIL`=?';
+	 $stmt=$conn->prepare( $sql );
+	 if( $stmt ){
+ 
+		 $stmt->bind_param("s",$_POST['contact_email']);
+		 $result=$stmt->execute();
+		 if( $result ){
+			 $stmt->store_result();
+			 $stmt->bind_result( $emailfound );
+			 $stmt->fetch();
+			 $stmt->free_result();
+		 }
+	 }
+ 
+	 if( $emailfound ){
+		 echo 'Sorry, that email address already exists in our database. Please try again with a different address.';
+		 $stmt->close();
+		 $conn->close();
+ 
+	 } else {
+    // Finally, register user if there are no errors in the form
+    	// $PASSWORD = md5($PASSWORD);//encrypt the PASSWORD before saving in the database
 
+    	$sbm = $conn->prepare("INSERT INTO users (FIRST_NAME, LAST_NAME, EMAIL, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+		$hashpass = password_hash($_POST['contact_password'], PASSWORD_DEFAULT);
+		$sbm->bind_param("ssssssss", $_POST['contact_name'], $_POST['contact_lname'], $_POST['contact_email'], $_POST['contact_address'], $_POST['contact_city'], $_POST['contact_state'], $_POST['contact_zipcode'], $hashpass);
+		$sbm->execute();
+    	// $_SESSION['success'] = "You are now logged in";
+		echo("Account created successfully!");
+    	header('location: Sign_up.php');
+    
 
+	if(isset($_POST['contact_email'])) {
+ 
+		// EDIT THE 2 LINES BELOW AS REQUIRED
+		$email_to_us = "wonderplannerofficial@gmail.com";
+		$email_to_user = $_POST['contact_email'];
+		$email_subject_user = "Thank you for registering at Wonder Planner!";
+		$email_subject_us = "New User Notification: $EMAIL";
+	
+	 
+		$name = $_POST['contact_name']; // required
+		$email_from = $_POST['contact_email']; // required
+		$email_message_user = "Hey!\n\nThank you for signing up! Here at Wonderplanner, we offer various event packages to help you have the best time at your event, communicate seamlessly between groups, and save money along the way! \n\nLooking forward to hear back from you soon! \n\nCheers, \nThe Wonder Planner Team";
+		$email_message_us = "A new user just registered!\n\n";
+	 
+		 
+		function clean_string($string) {
+		  $bad = array("content-type","bcc:","to:","cc:","href");
+		  return str_replace($bad,"",$string);
+		}
+	 
+		 
+	 
+		$email_message .= "Name: ".clean_string($name)."\n";
+		$email_message .= "Email: ".clean_string($email_from)."\n";
+	 
+	// send email to user
+	$headers = 'From: '.$email_to_us."\r\n".
+	'Reply-To: '.$email_to_us."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	@mail($email_to_user, $email_subject_user, $email_message_user, $headers);  
 
-		<!-- Scripts -->
-			<script>
-			  (function(b,i,t,C,O,I,N) {
-				window.addEventListener('load',function() {
-				  if(b.getElementById(C))return;
-				  I=b.createElement(i),N=b.getElementsByTagName(i)[0];
-				  I.src=t;I.id=C;N.parentNode.insertBefore(I, N);
-				},false)
-			  })(document,'script','https://widgets.bitcoin.com/widget.js','btcwdgt');
-			</script>
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/skel-viewport.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-			<!--===============================================================================================-->
-				<script src="assets/Login_v1/vendor/jquery/jquery-3.2.1.min.js"></script>
-			<!--===============================================================================================-->
-				<script src="assets/Login_v1/vendor/bootstrap/js/popper.js"></script>
-				<script src="assets/Login_v1/vendor/bootstrap/js/bootstrap.min.js"></script>
-			<!--===============================================================================================-->
-				<script src="assets/Login_v1/vendor/select2/select2.min.js"></script>
+	// send email to us
+	// $headers = 'From: '.$email_from."\r\n".
+	'Reply-To: '.$email_from."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	@mail($email_to_us, $email_subject_us, $email_message_us, $headers);  
+	
+	?>
+	 
+	<!-- include your own success html here -->
+	 
+	Thank you for Signing up!
+	 
+	<?php
+	 
+	}
+}
 
-			<!--===============================================================================================-->
-				<script src="assets/Login_v1/js/main.js"></script>
-			<script src="/socialshare/scripts/jquery.socialshareprivacy.min.autoload.js"></script>
+//   if ($conn->query(conn) === TRUE) {
+//       echo "New record created successfully";
+//   } else {
+//       echo "Error: " . conn . "<br>" . $conn->error;
+//   }
+}
 
-	</body>
-</html>
+//   $conn->close();
+?>
